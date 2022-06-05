21dae7b4ec63d7349d632e549d3e9026b71c87f8
Somen Singha <somensingha06@gmail.com>
2022-06-05 14:26:06 +0600

    added

---
+++ shopping.js
@@ -0,0 +1,31 @@
+const addItem = () => {
+  const nameField = document.getElementById("product-name");
+  const name = nameField.value;
+  displayProduct(name);
+  addProductToCart(name);
+
+  nameField.value = "";
+};
+
+const displayProduct = (name) => {
+  console.log(name);
+  const ul = document.getElementById("products");
+  const li = document.createElement("li");
+  li.innerText = name;
+  ul.appendChild(li);
+};
+const getCart = () => {
+  const cart = localStorage.getItem("cart");
+  let cartObj;
+  if (cart) {
+    cartObj = JSON.parse(cart);
+  } else {
+    cartObj = {};
+  }
+  return cartObj;
+};
+const addProductToCart = () => {
+  const cart = getCart();
+  cart[name] = 1;
+  const cartStringified = JSON.stringify(cart);
+};
